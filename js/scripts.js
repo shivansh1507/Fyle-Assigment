c9858d028ece428c4cb121dff9f4f822077c3b60
Shivansh Sharma <sharmashivansh462@gmail.com>
2024-06-02 18:52:18 +0530

    Main
    Main Commit

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,28 @@ $(document).ready(function() {
     });
   });
   
+  $(document).ready(function () {
+    var images = ['img/1-2.png', 'img/2-2.png', 'img/3-2.png'];
+    var currentIndex = 0;
+
+    function updateImage(index) {
+        $('#carouselImage').attr('src', images[index]);
+        updateDots(index);
+    }
+
+    function updateDots(index) {
+        $('.custom-indicators .dot').removeClass('active').attr('src', 'img/2.svg');
+        $('.custom-indicators .dot').eq(index).addClass('active').attr('src', 'img/1.svg');
+    }
+
+    $('.custom-indicators .dot').click(function () {
+        currentIndex = $(this).data('slide-to');
+        updateImage(currentIndex);
+    });
+
+    $('#carouselImage').hover(function() {
+        $(this).css('transform', 'scale(1.2)');
+    }, function() {
+        $(this).css('transform', 'scale(1)');
+    });
+});
